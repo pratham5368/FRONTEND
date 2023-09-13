6a1fe49f9a4e7e91562319da04d69d74d5ceab08
pratham rai <97867973+pratham5368@users.noreply.github.com>
2023-09-14 01:20:41 +0530

    SANDBOX

---
+++ 07-typescript/01-intro/01-Intro.ts
@@ -0,0 +1,6 @@
+//TypeScript is a syntactic superset of JavaScript which adds static typing.
+
+//This basically means that TypeScript adds syntax on top of JavaScript, allowing developers to add types.
+
+
+//https://github.com/TypeStrong/ts-node#commonjs-vs-native-ecmascript-modules
